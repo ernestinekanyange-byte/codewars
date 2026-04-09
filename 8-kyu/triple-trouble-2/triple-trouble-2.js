6596e660db9e1ecc315a082f08194ac8a457497a
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-04-09 20:20:16 +0200

    Update solution - CodeHub

--- 8-kyu/triple-trouble-2/triple-trouble-2.js
+++ 8-kyu/triple-trouble-2/triple-trouble-2.js
@@ -1,17 +1,17 @@
 ​
-  //Solution
+  
  function tripleTrouble(one, two, three) {
   let result = "";
   
   for (let i = 0; i < one.length; i++) {
-    // Append the i-th character from each string in order
+    
     result += one[i] + two[i] + three[i];
   }
   
   return result;
 }
 ​
-// Example usage:
+​
 console.log(tripleTrouble("aa", "bb", "cc")); 
 ​
 ​
