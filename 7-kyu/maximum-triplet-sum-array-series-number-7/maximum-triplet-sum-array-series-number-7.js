5d01a917e8ed284fe345d414058a6be23df32275
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-04-10 23:30:13 +0200

    Add solution - CodeHub

---
+++ 7-kyu/maximum-triplet-sum-array-series-number-7/maximum-triplet-sum-array-series-number-7.js
@@ -0,0 +1,17 @@
+function maxTriSum(numbers){
+  let unique = [...new Set(numbers)];
+  // I used this method for removing suplicates before sorting
+let sorted = unique.sort ((a,b) => b-a);
+  let sum = sorted [0]+ sorted [1] + sorted [2];
+  
+  return sum;
+  }
+console.log(maxTriSum([3,5,2,6,1]));
+  
+  
+  
+​
+​
+​
+​
+​
