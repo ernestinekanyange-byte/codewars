6d4b1be4780e41898e8279915209f4ca9676a672
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-04-12 22:10:58 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-array-mean/grasshopper-array-mean.js
@@ -0,0 +1,16 @@
+function findAverage(array){
+  
+   let sum = 0
+   for (let i = 0; i< array.length; i++){
+     sum +=array[i];
+     
+   }
+  
+   return average = sum /array.length
+  
+    }
+        
+ console.log(findAverage([1,3,5,7]))
+  
+  
+  
