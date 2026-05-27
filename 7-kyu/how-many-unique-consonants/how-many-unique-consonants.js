ef88c3609cc0b2c7f61c9aff01cd4f806c6fb49a
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-05-28 00:00:18 +0200

    Update solution - CodeHub

--- 7-kyu/how-many-unique-consonants/how-many-unique-consonants.js
+++ 7-kyu/how-many-unique-consonants/how-many-unique-consonants.js
@@ -1,12 +1,3 @@
 function countConsonants(str) {
-  const vowels = "aeiou";
-  const consonants = new Set();
-​
-  for (const char of str.toLowerCase()) {
-    if (/[a-z]/.test(char) && !vowels.includes(char)) {
-      consonants.add(char);
-    }
-  }
-​
-  return consonants.size;
+  return new Set(str.toLowerCase().replace(/[^a-z]|[aeiou]/g,"")).size
 }
