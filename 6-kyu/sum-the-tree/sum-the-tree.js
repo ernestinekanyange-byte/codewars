e9a235d861ce6817c0351c694c9e9e00f829bb4e
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-06-17 15:59:00 +0200

    Add solution - CodeHub

---
+++ 6-kyu/sum-the-tree/sum-the-tree.js
@@ -0,0 +1,11 @@
+function sumTheTreeValues(root) {
+  if (root === null) {
+    return 0;
+  }
+​
+  return (
+    root.value +
+    sumTheTreeValues(root.left) +
+    sumTheTreeValues(root.right)
+  );
+}
