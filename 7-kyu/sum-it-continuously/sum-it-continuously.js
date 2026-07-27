7ccbe0a7969c3652f9e1c8f8ef8bd8f06a9a70b0
Ernestine Kanyange <ernestinekanyange@gmail.com>
2026-07-27 17:41:09 +0200

    Add solution - CodeHub

---
+++ 7-kyu/sum-it-continuously/sum-it-continuously.js
@@ -0,0 +1,11 @@
+function add(arr) {
+  let sum = 0;
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+    
+    result.push(sum)
+    }
+    return result ;
+    }
+​
